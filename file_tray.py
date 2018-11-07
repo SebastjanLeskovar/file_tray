f1230ba2be45f02dda07b2400b5e61e30890c0ef
Sebastjan <sebastjan.leskovar@gmail.com>
2018-11-07 18:52:36 +0100

    V2.51

--- file_tray.py
+++ file_tray.py
@@ -1,24 +1,14 @@
 import os
 import json
 import sys
-import datetime
+import logging
 
 service_selection = None
 
-def get_timestamp():
-    timestamp = datetime.datetime.now().replace(microsecond=0)
-    timestamp.strftime('%Y/%m/%d %H:%M:%S')
-    return str(timestamp) + " - "
-
-def write_to_log(text):
-    log = open('Log.txt', 'a')
-    log.write(get_timestamp())
-    log.write(text)
-    log.write("\n")
-    log.close()
+log = logging.Logging()
 
 def ask_for_service():
-    write_to_log("Program File Tray started.")
+    log.write_to_log("Program File Tray started.")
     print("Welcome to program File Tray.")
     
     options = "1. Translation only\n2. Translation with revision\n3. Proofreading\n4. Cancel\nPlease enter 1, 2, 3 or 4: "
@@ -38,7 +28,7 @@ def ask_for_service():
         service_selection = "proofreading"
     elif number_input == "4":
         print("The program is terminated.")
-        write_to_log("The program was terminated with sys.exit().")
+        log.write_to_log("The program was terminated with sys.exit().")
         sys.exit()
 
 def create_project_folder_structure():
@@ -53,7 +43,7 @@ def create_project_folder_structure():
     
         os.makedirs(project_folder_location)
         print("The project folder '%s' was created." % project_folder_name)
-        write_to_log("The project folder '%s' was created." % project_folder_name)
+        log.write_to_log("The project folder '%s' was created." % project_folder_name)
 
         config["project_number"] += 1
     
@@ -61,14 +51,14 @@ def create_project_folder_structure():
             os.makedirs(os.path.join(config["root_directory"], project_folder_name, service))
         
         print("The folders for service '%s' were created." % service_selection)
-        write_to_log("The folders for service %s were created in project folder '%s'." % (service_selection, project_folder_name))
+        log.write_to_log("The folders for service %s were created in project folder '%s'." % (service_selection, project_folder_name))
 
         with open("config.json", "w") as json_file:
             json.dump(config, json_file, indent=4)
 
     else:
         print("The folder '%s' already exists at that location. Please check the project folders and change the 'project_number' value in Config accordingly. " % project_folder_name)
-        write_to_log("The folder '%s' was found at the location. The project folder was not created." % project_folder_name)
+        log.write_to_log("The folder '%s' was found at the location. The project folder was not created." % project_folder_name)
     
 
 ask_for_service()
